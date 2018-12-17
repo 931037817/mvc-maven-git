d5abcbda881afbf36181bb41b0fe79c4f7c351a0
931037817 <931037817@qq.com>
2018-12-17 16:29:07 +0800

    叮咚十

--- src/main/java/com/zy/controller/BaseController.java
+++ src/main/java/com/zy/controller/BaseController.java
@@ -9,7 +9,7 @@ import org.apache.commons.logging.LogFactory;
 
 public class BaseController {
 	
-	// 日志......叮咚九
+	// 日志......叮咚十
 	protected final Log log = LogFactory.getLog(this.getClass());
 	protected final String className = this.getClass().getName();
 	
