9baccf3f0f4ef879513c6094d52001b3266d4125
931037817 <931037817@qq.com>
2018-12-17 14:42:18 +0800

    不改了不改了

--- src/main/java/com/zy/controller/BaseController.java
+++ src/main/java/com/zy/controller/BaseController.java
@@ -9,7 +9,7 @@ import org.apache.commons.logging.LogFactory;
 
 public class BaseController {
 	
-	// 日志.....！！！
+	// 日志.....！！！......
 	protected final Log log = LogFactory.getLog(this.getClass());
 	protected final String className = this.getClass().getName();
 	
